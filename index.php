69d5f641bc3211e3b60d411aa4904fa56aec53c7
midoelhawy <android.com111@gmail.com>
2019-02-24 00:07:21 +0100

    update err

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
 <?php
-$dir    = 'Multango/';
-$arrayoldwords = array("Mulilanguage","mulilanguage","MULTILANGUAGE","MULTILANGUAGE_");
-$arraynewwords = array("Multango","multango","MULTANGO","MULTANGO_");
+$dir    = 'midosat/';
+$arrayoldwords = array("total","Total","TOTAL","TOTAL_");
+$arraynewwords = array("midosat","Midosat","MIDOSAT","MIDOSAT_");
 $file_suc_edit = 0;
 $dir_read	   = 0;
 $tot_size      = 0;
-
-
-
-
 echo "<body style=' background: #000;color: #fff; '>";
 	echo "<ul>";
 rnm($dir);
-
-
-
-
-
-
 function rnm($dirct){
 	global $tot_size,$dir_read,$file_suc_edit,$arrayoldwords,$arraynewwords;
 	$files = scandir($dirct);
 	foreach($files as $file){
-	if(is_dir($dirct.$file) && ($file != "." && $file != "..")){
-		rnm($dirct.$file);
+	if(is_dir($dirct."/".$file) && ($file != "." && $file != "..")){
+		rnm($dirct."/".$file);
 		$dir_read++;
+		
+		echo "We Are In ".$dirct."/".$file;
 	}else{
 			$oldfile = $file;
-		if(($file != "." && $file != ".." && @!is_array(getimagesize($dirct.$oldfile)))){
-			$fname = $dirct.$oldfile;
+			
+			if(@is_array(getimagesize($oldfile))){
+				$image = true;
+			} else {
+				$image = false;
+			}
+			
+		if(($file != "." && $file != ".." && $image == false)){
+			$fname = $dirct."/".$oldfile;
 			$fhandle = @fopen($fname,"r");
 			
 			$content = @fread($fhandle,@filesize($fname));
@@ -37,7 +36,6 @@ function rnm($dirct){
 				$content = str_replace($val,$arraynewwords[$kay], $content);
 			}
 				
-
 			$fhandle = fopen($fname,"w");
 			fwrite($fhandle,$content);
 			fclose($fhandle);
@@ -60,5 +58,4 @@ function rnm($dirct){
 	echo "<br><hr><br> TOTAL SIZE         : ".round(($tot_size/1024), 2)." KB<br><br><hr><br>";
 	
 echo "</body>";
-
 ?>
